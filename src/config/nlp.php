e6cb5f2cadc09bf7afcdd5a547e45ade49a5dd09
Olav <olav@web64.com>
2018-04-12 13:39:43 +0200

    configs

--- src/config/nlp.php
+++ src/config/nlp.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
+    // See instructions for setting up the NLP Server here: https://github.com/web64/nlpserver
     'hosts' => [
         'http://localhost:6400/',
-    //  'http://server2:6400/',
+    //  'http://server2:6400/', 
     ],
 
 
